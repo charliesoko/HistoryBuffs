0f3688587f1b48f477719df314737aebcc2c43de
Gustavo Molina <72176060+childofprophecy@users.noreply.github.com>
2024-02-21 11:48:41 -0600

    Update2

--- Assets/Scripts/CharacterMenu.cs
+++ Assets/Scripts/CharacterMenu.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class CharacterMenu : MonoBehaviour
 {
 
-    private string characterName;
+    private string P1characterName = "Picking...";
+
+    private string P2characterName = "Picking...";
 
     private bool p1Turn = true;
 
@@ -14,19 +19,48 @@ public class CharacterMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log($"Player 1 is: {P1characterName}");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (p1Turn == true && p2Turn == false)
+            {
+                SceneManager.LoadScene("MainMenu");
+            }
+            else if (p1Turn == false && p2Turn == true)
+            {
+                p1Turn = true;
+                p2Turn = false;
+                P1characterName = "Picking...";
+                Debug.Log($"Player 1 is: {P1characterName}");
+            }
+        }
+
     }
 
-    public void P1Select()
+    public void PlayerSelect()
     {
-        characterName = gameObject.name;
-        p1Turn = false;
-        p2Turn = true;
+        if (p2Turn == true)
+        {
+            P2characterName = EventSystem.current.currentSelectedGameObject.name;
+            p2Turn = false;
+            Debug.Log($"Player 2 is: {P2characterName}");
+            SceneManager.LoadScene("SampleScene");
+        }
+
+        if (p1Turn == true)
+        {
+            P1characterName = EventSystem.current.currentSelectedGameObject.name;
+            p1Turn = false;
+            p2Turn = true;
+
+            Debug.Log($"Player 1 is: {P1characterName}");
+        }
+
     }
 }
