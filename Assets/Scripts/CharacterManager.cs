71a3186b76babc9c95e522cd9a73614a6cd5f131
charliesoko <56273909+charliesoko@users.noreply.github.com>
2024-02-28 11:42:24 -0600

    Systems work
    Started work on a CharacterManager script that will help manage players in the game as well as the different characters that can be selected by players

---
+++ Assets/Scripts/CharacterManager.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterManager : MonoBehaviour
+{
+    public int numberOfUsers;
+    public List<PlayerBase> players = new List<PlayerBase>();
+
+    public List<CharacterBase> characterList = new List<CharacterBase>();
+
+    public CharacterBase returnCharacterWithID(string id)
+    {
+        CharacterBase returnValue = null;
+
+        for (int i= 0; i < characterList.Count; i++)
+        {
+            if (string.Equals(characterList[i].charID,id))
+            {
+                returnValue = characterList[i];
+            }
+        }
+
+        return returnValue;
+    }
+
+    public PlayerBase returnPlayerFromStates(StateManager states)
+    {
+        PlayerBase returnValue = null;
+
+        for (int i = 0; i < players.Count; i++)
+        {
+            if(players[i].playerStates == states)
+            {
+                returnValue = players[i];
+                break;
+            }
+        }
+
+        return returnValue;
+    }
+
+    // CharacterManager class is a singleton, can be accessed anywhere by calling GetInstance function below
+    public static CharacterManager instance;
+    public static CharacterManager GetInstance()
+    {
+        return instance;
+    }
+
+    // Set up instance in Awake, also set instance to carry over between scenes
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
+
+[System.Serializable]
+public class PlayerBase
+{
+    public string playerID;
+    public string inputID;
+    public PlayerType playerType;
+    public bool hasCharacter;
+    public GameObject playerPrefab;
+    public StateManager playerStates;
+    public int score;
+}
+
+[System.Serializable]
+public class CharacterBase
+{
+    public string charID;
+    public GameObject prefab;
+}
